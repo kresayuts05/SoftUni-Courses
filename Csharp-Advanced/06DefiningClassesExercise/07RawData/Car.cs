56887b1b995bc491c24e1f6ce291d1ed3b21841e
kresayuts05 <kresatsvetkova05@gmail.com>
2021-02-05 23:31:40 +0200

    Upload new tasks

--- Csharp-Advanced/06DefiningClassesExercise/07RawData/Car.cs
+++ Csharp-Advanced/06DefiningClassesExercise/07RawData/Car.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _07RawData
+namespace DefiningClasses
 {
-    class Car
+    public class Car
     {
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+        public Car(string model, Engine engine, Cargo cargo, Tire[] tires)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+            Tires = tires;
+        }
     }
 }
--- Csharp-Advanced/06DefiningClassesExercise/07RawData/Program.cs
+++ Csharp-Advanced/06DefiningClassesExercise/07RawData/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace _07RawData
+namespace DefiningClasses
 {
-    class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string[] carInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = carInfo[0];
+                int engineSpeed = int.Parse(carInfo[1]);
+                int enginePower = int.Parse(carInfo[2]);
+
+                var engine = new Engine(engineSpeed, enginePower);
+
+                int cargoWeight = int.Parse(carInfo[3]);
+                string cargoType = carInfo[4];
+
+                var cargo = new Cargo(cargoWeight, cargoType);
+
+                var tires = new Tire[4]
+                {
+                    new Tire(double.Parse(carInfo[5]), int.Parse(carInfo[6])),
+                    new Tire(double.Parse(carInfo[7]), int.Parse(carInfo[8])),
+                    new Tire(double.Parse(carInfo[9]), int.Parse(carInfo[10])),
+                    new Tire(double.Parse(carInfo[11]), int.Parse(carInfo[12]))
+                };
+
+                Car car = new Car(model, engine, cargo, tires);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                var result = cars
+                    .Where(x => x.Cargo.Type == "fragile")
+                    .Where(x => x.Tires.Any(y => y.Pressure < 1))
+                    .ToList();
+
+                foreach (var car in result)
+                {
+                    Console.WriteLine(car.Model);
+                }
+            }
+            else if (command == "flamable")
+            {
+                var result = cars
+                    .Where(x => x.Cargo.Type == "flamable")
+                    .Where(x => x.Engine.Power > 250)
+                    .ToList();
+
+                foreach (var car in result)
+                {
+                    Console.WriteLine(car.Model);
+                }
+            }
         }
     }
 }
