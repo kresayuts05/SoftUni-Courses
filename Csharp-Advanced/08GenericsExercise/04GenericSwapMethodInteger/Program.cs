6da75b1d5d9da01f754009a13388079f0be97a2a
kresayuts05 <kresatsvetkova05@gmail.com>
2021-02-13 21:34:46 +0200

    Upload new tasks

--- Csharp-Advanced/08GenericsExercise/04GenericSwapMethodInteger/Program.cs
+++ Csharp-Advanced/08GenericsExercise/04GenericSwapMethodInteger/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _04GenericSwapMethodInteger
 {
-    class Program
+    class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            string[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int firstIndex = int.Parse(indexes[0]);
+            int secondIndex = int.Parse(indexes[1]);
+
+            GenericClass<int> generic = new GenericClass<int>();
+
+            List<int> output = generic.Swap(list, firstIndex, secondIndex);
+
+            foreach (var item in output)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
         }
     }
 }
+    
