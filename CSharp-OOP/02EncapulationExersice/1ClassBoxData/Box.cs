c2071448bea1da03b4c7e72e9d1320f66dc1bf7a
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-06 20:46:56 +0200

    Upload new tasks

---
+++ CSharp-OOP/02EncapulationExersice/1ClassBoxData/Box.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1ClassBoxData
+{
+    public class Box
+    {
+        private double lenght;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+        }
+
+        public double Lenght
+        {
+            get
+            {
+                return lenght;
+            }
+            private set
+            {
+                if (lenght <= 0)
+                {
+                    throw new ArgumentException("Lenght cannot be zero or negative.");
+                    lenght = value;
+                }
+
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (width <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                    width = value;
+                }
+
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (height <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                    height = value;
+                }
+
+            }
+        }
+
+        public void SurfaceArea()
+        {
+            double surfaceArea = 2 * Lenght * Width + 2 * Lenght * Height + 2 * Width * Height;
+            Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+        }
+
+        public void LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * Lenght * Height + 2 * Width * Height;
+
+            Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+        }
+
+        public void Volume()
+        {
+            double volume = Lenght * Width * Height;
+            Console.WriteLine($"Volume - {volume:f2}");
+        }
+    }
+}
---
+++ CSharp-OOP/02EncapulationExersice/1ClassBoxData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1ClassBoxData
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int lenght = int.Parse(Console.ReadLine());
+                int width = int.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                Box box = new Box(lenght, width, height);
+
+                box.SurfaceArea();
+                box.LateralSurfaceArea();
+                box.Volume();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
