4af9001fec11d1d38daa7972379b2d9ee114a04f
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-07 22:56:03 +0200

    Upload new tasks

--- CSharp-OOP/02EncapulationExersice/03ShoppingSpree/Product.cs
+++ CSharp-OOP/02EncapulationExersice/03ShoppingSpree/Product.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace _03ShoppingSpree
 {
-    class Product
+    public class Product
     {
+        private string name;
+        private int cost;
+
+        public Product(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+
+        }
+
+        public int Cost
+        {
+            get => cost;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                cost = value;
+            }
+        }
     }
 }
