35b93d851d3784ed330114e95c5accc318884839
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-15 20:14:25 +0200

    Upload new tasks

---
+++ CSharp-OOP/03InterfacesAndAbstractionExercise/07MilitaryElite/Commando.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07MilitaryElite
+{
+    class Commando : ISpecialisedSoldier
+    {
+        public string Crop { get; private set; }
+
+        public int ID { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        List<Mission> missions { get; set; }
+    }
+}
---
+++ CSharp-OOP/03InterfacesAndAbstractionExercise/07MilitaryElite/Engineer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07MilitaryElite
+{
+    public class Engineer : ISpecialisedSoldier
+    {
+        public string Crop { get; private set; }
+
+        public int ID { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public List<Repair> Repairs { get; private set; }
+
+
+    }
+}
