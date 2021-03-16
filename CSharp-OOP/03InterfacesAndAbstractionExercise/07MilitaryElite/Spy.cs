ddf3b44a56a4c819f66142c5f5e40e6ce04c8c31
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-16 06:29:59 +0200

    Upload new tasks

---
+++ CSharp-OOP/03InterfacesAndAbstractionExercise/07MilitaryElite/Spy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07MilitaryElite
+{
+    public class Spy : ISpy
+    {
+        public int ID { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Code { get; private set; }
+    }
+}
