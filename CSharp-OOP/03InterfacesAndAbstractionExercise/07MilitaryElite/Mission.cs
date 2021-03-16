e948659f885b21a7c9b5e4cf0c028655c6c8224e
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-16 06:29:39 +0200

    Upload new tasks

---
+++ CSharp-OOP/03InterfacesAndAbstractionExercise/07MilitaryElite/Mission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07MilitaryElite
+{
+    public class Mission
+    {
+        public string CodeName { get; set; }
+
+        public string State { get; set; }
+
+        public void CompleteMission()
+        {
+            State = "Finished";
+        }
+    }
+}
