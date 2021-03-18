81199295aa631153179a126cc7032965dca33e46
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-18 20:07:45 +0200

    Upload new tasks

---
+++ CSharp-OOP/04PolymorphismExercise/01Vehicles/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01Vehicles
+{
+    public abstract class Vehicle
+    {
+        public int FuelQuantity { get;}
+
+        public double FuelConsumption { get;}
+
+        public virtual void Drive(int distance)
+        {
+            Console.WriteLine($"{} travelled {distance} km");
+        }
+    }
+}
