4e49c54f4c7a5349444f4ea74000c280150332f6
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-19 22:52:31 +0200

    Upload new tasks

---
+++ CSharp-OOP/04PolymorphismExercise/01Vehicles/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01Vehicles
+{
+    public class Car : Vehicle
+    {
+        private const double airConditionar = 0.9;
+
+        public Car(double fuelQuantity, double fuelConsumation, double tankCapacity) 
+            : base(fuelQuantity, fuelConsumation, airConditionar, tankCapacity)
+        {
+        }
+    }
+}
