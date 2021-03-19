ffa765a8e3f354043cfbd896eb20556ebf1d54a2
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-19 22:51:58 +0200

    Upload new tasks

--- CSharp-OOP/04PolymorphismExercise/01Vehicles/Vehicle.cs
+++ CSharp-OOP/04PolymorphismExercise/01Vehicles/Vehicle.cs
@@ -6,13 +6,69 @@ namespace _01Vehicles
 {
     public abstract class Vehicle
     {
-        public int FuelQuantity { get;}
+        private double fuelQuantity;
 
-        public double FuelConsumption { get;}
-
-        public virtual void Drive(int distance)
+        protected Vehicle(double fuelQuantity, double fuelConsumation, double airConditioner, double tankCapacity)
         {
-            Console.WriteLine($"{} travelled {distance} km");
+            FuelQuantity = fuelQuantity;
+            TankCapacity = tankCapacity;
+            FuelConsumation = fuelConsumation;
+            AirConditionar = airConditioner;
+        }
+
+        public double FuelQuantity 
+        { 
+            get => fuelQuantity;
+            protected set
+            {
+                if(value > TankCapacity)
+                {
+                    fuelQuantity = 0;
+                }
+                else
+                {
+                    fuelQuantity = value;
+                }
+            }
+        }
+
+        public double FuelConsumation { get; private set; }
+
+        public double TankCapacity { get; private set; }
+
+        private double AirConditionar { get; set; }
+
+        public void Drive(double distance)
+        {
+            double needFuel = (FuelConsumation + AirConditionar) * distance;
+
+            if (needFuel > FuelQuantity)
+            {
+                throw new ArgumentException($"{GetType().Name} needs refueling");
+            }
+
+            FuelQuantity -= needFuel;
+        }
+
+        public virtual void Refuel(double fuel)
+        {
+            if (fuel <= 0)
+            {
+                throw new InvalidOperationException("Fuel must be a positive number");
+            }
+
+            if (fuel + FuelQuantity > TankCapacity)
+            {
+                FuelQuantity = 0;
+                throw new InvalidOperationException($"Cannot fit {fuel + FuelQuantity} fuel in the tank");
+            }
+
+            FuelQuantity += fuel;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}: {FuelQuantity:F2}";
         }
     }
 }
