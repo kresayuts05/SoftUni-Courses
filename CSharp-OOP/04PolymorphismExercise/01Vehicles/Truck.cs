f6b8f1a680b57a1b3a1295913ab6ea4e8b745ee1
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-19 22:53:11 +0200

    Upload new tasks

---
+++ CSharp-OOP/04PolymorphismExercise/01Vehicles/Truck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01Vehicles
+{
+    public class Truck : Vehicle
+    {
+        private const double airConditionar = 1.6;
+
+        public Truck(double fuelQuantity, double fuelConsumation, double tankCapacity)
+            : base(fuelQuantity , fuelConsumation, airConditionar, tankCapacity)
+        {
+        }
+
+        public override void Refuel(double fuel)
+        {
+            base.Refuel(fuel * 0.95);
+        }
+    }
+}
