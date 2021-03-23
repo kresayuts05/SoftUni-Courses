975850c2e29ea9a5bc2fc8a25bae55789a3d1351
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-23 22:34:01 +0200

    Upload new tasks

---
+++ CSharp-OOP/04PolymorphismExercise/03Raiding/Rogue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03Raiding
+{
+    public class Rogue : BaseHero
+    {
+        private const int power = 80;
+
+        public Rogue(string name) 
+            : base(name, power)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{nameof(Rogue)} - {Name} hit for {Power} damage";
+        }
+    }
+}
