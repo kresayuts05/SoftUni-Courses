ddbf1e1af3a704e1060efd43b5a4a73cceba7dea
kresayuts05 <kresatsvetkova05@gmail.com>
2021-03-31 22:07:13 +0300

    Upload new tasks

---
+++ CSharp-OOP/04PolymorphismExercise/04WildFarm/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04WildFarm
+{
+    public abstract class Bird : Animal
+    {
+        protected Bird(string name, double weight, double wingSize) 
+            : base(name, weight)
+        {
+            WingSize = wingSize;
+        }
+
+        public  double WingSize { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [{Name}, {WingSize}, {Math.Round(Weight, 2)}, {FoodEaten}]";
+        }
+    }
+}
