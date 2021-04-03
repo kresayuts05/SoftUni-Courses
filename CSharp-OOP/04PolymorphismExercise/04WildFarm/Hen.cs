848432cd774052edc69e0c5ae13a5ba390112a8e
kresayuts05 <kresatsvetkova05@gmail.com>
2021-04-03 23:35:38 +0300

    Upload new tasks

---
+++ CSharp-OOP/04PolymorphismExercise/04WildFarm/Hen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04WildFarm
+{
+    public class Hen : Bird
+    {
+        public Hen (string name, double weight, double wingSize)
+             : base(name, weight, wingSize)
+        {
+        }
+
+        public override void AskingFood()
+        {
+            Console.WriteLine("Cluck");
+        }
+
+        public override void Eating(string foodType, int quantity)
+        {
+            FoodEaten += quantity;
+            Weight +=  0.35 * quantity;
+        }
+    }
+}
