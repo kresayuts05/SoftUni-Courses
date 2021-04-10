178e0256ce037999563eec182b0294e6abfe7370
kresayuts05 <kresatsvetkova05@gmail.com>
2021-04-10 13:07:46 +0300

    Upload new tasks

---
+++ CSharp-OOP/Exams/12-December-2020-Exam/01. Structure_Problem_Skeleton/Bakery/Models/BakedFoods/Bread.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.BakedFoods
+{
+    public class Bread : BakedFood
+    {
+        private const int portion = 200;
+
+        public Bread(string name, decimal price) 
+            : base(name, portion, price)
+        {
+        }
+    }
+}
---
+++ CSharp-OOP/Exams/12-December-2020-Exam/01. Structure_Problem_Skeleton/Bakery/Models/BakedFoods/Cake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.BakedFoods
+{
+    public class Cake : BakedFood
+    {
+        private const int portion = 245;
+
+        public Cake(string name, decimal price) 
+            : base(name, portion, price)
+        {
+        }
+    }
+}
