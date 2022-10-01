8b02f1e900210b92e8bdf15db323c6ba661ce4e0
kresa <kresayuts@gmail.com>
2022-10-01 23:02:56 +0300

    Uploading new course

---
+++ C#-ASP.NET-Fundamentals/SimpleWebChat/SimpleWebChat/Controllers/ChatController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebChat.Models;
+
+namespace SimpleWebChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> Messages = new List<KeyValuePair<string, string>>();
+
+        public IActionResult Show()
+        {
+            if (Messages.Count() < 1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = Messages.Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                }).ToList()
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            var newMessages = chat.CurrentMessage;
+
+            Messages.Add(new KeyValuePair<string, string>(newMessages.Sender, newMessages.MessageText));
+
+            return RedirectToAction("Show");
+        }
+    }
+}
